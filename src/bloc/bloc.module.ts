03a82cd179df0e9fb8f502f42cc3cd747c76df3c
mohamed-akacha <benakachamohamed11@gmail.com>
2023-03-28 21:50:41 +0100

    fixes

--- src/bloc/bloc.module.ts
+++ src/bloc/bloc.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { BlocService } from './bloc.service';
 import { BlocController } from './bloc.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlocEntity } from './entities/bloc.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthGuard } from 'src/user/guards/jwt-auth.guard';
+import { RoleGuard } from 'src/user/guards/rôles.guard';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([BlocEntity]),
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+        secret:process.env.SECRET,
+        signOptions: {
+          expiresIn: 3600
+        }
+      })
+  ],
   controllers: [BlocController],
-  providers: [BlocService],
+  providers: [BlocService , RoleGuard ,JwtAuthGuard ],
   exports:[BlocService]
 })
 export class BlocModule {}
--- src/evaluation/evaluation.module.ts
+++ src/evaluation/evaluation.module.ts
@@ -6,6 +6,7 @@ import { BlocModule } from 'src/bloc/bloc.module';
 import { EvaluationPointModule } from 'src/evaluation-point/evaluation-point.module';
 import { EvaluationPointService } from 'src/evaluation-point/evaluation-point.service';
 import { InspectionModule } from 'src/inspection/inspection.module';
+import { UserEntity } from 'src/user/entities/user.entity';
 import { RoleGuard } from 'src/user/guards/rôles.guard';
 import { JwtStrategy } from 'src/user/strategy/passport-jwt.strategy';
 import { UserModule } from 'src/user/user.module';
@@ -15,7 +16,7 @@ import { EvaluationService } from './evaluation.service';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([EvaluationEntity,]),
+    TypeOrmModule.forFeature([EvaluationEntity,UserEntity]),
     PassportModule.register({
       defaultStrategy: 'jwt'
     }),
@@ -25,9 +26,10 @@ import { EvaluationService } from './evaluation.service';
           expiresIn: 3600
         }
       }),
-    UserModule, InspectionModule ,BlocModule ,EvaluationPointService
+    UserModule, InspectionModule ,BlocModule ,EvaluationPointModule
   ],
   controllers: [EvaluationController],
-  providers: [EvaluationService, JwtStrategy, RoleGuard]
+  providers: [EvaluationService, JwtStrategy, RoleGuard],
+  exports:[EvaluationService]
 })
 export class EvaluationModule {}
